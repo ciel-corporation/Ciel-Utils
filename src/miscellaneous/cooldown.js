ea4dbde9a5bf2ed6a9630fac12883dd076772236
Glitch (one-who-helps-friends) <none>
2022-08-08 10:47:43 +0000

    fixing the cooldown function

--- src/miscellaneous/cooldown.js
+++ src/miscellaneous/cooldown.js
@@ -2,13 +2,14 @@
  * Function used to calculate cooldowns
  * @params {number} timestamp - the last time it was used
  * @params {number} rechargeTime - the recharge time
+ * @returns {array} the first says if it passed the second is the result of the cooldown
  */
 
 function cooldown(timestamp, rechargeTime) {
   const time = rechargeTime - (Date.now() - timestamp);
 
-  if (timestamp !== null && time > 0) return false;
-  else return true;
+  if (timestamp !== null && time > 0) return [false, time];
+  else return [true, time];
 }
 
 module.exports = cooldown;
